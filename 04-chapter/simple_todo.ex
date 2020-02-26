4def16ef417fa2f7ce47e95fa7d582340e632ef1
Herminio Torres <herminiocesar@gmail.com>
2020-02-26 18:27:49 -0300

    add the add_entry function who add a new item on the to-do list

--- 04-chapter/simple_todo.ex
+++ 04-chapter/simple_todo.ex
@@ -1,3 +1,12 @@
 defmodule TodoList do
   def new(), do: %{}
+
+  def add_entry(todo_list, date, title) do
+    Map.update(
+      todo_list,
+      date,
+      [title],
+      fn titles -> [title | titles] end
+    )
+  end
 end
